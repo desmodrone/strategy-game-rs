10dabe0ee460b1c3e6251faa57612c746e98710e
desmodrone <62178867+desmodrone@users.noreply.github.com>
2023-04-24 14:01:40 +0200

    add assets to project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::prelude::*;
+
+fn create_battlefield_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    const NUM_COLUMNS: usize = 20;
+    const NUM_ROWS: usize = 20;
+    const TILE_SIZE: f32 = 16.0;
+
+    let tiles_handle = asset_server.load("Tiles/FullTileset.png");
+    let tiles_atlas = TextureAtlas::from_grid(
+        tiles_handle,
+        Vec2::new(TILE_SIZE, TILE_SIZE),
+        NUM_COLUMNS,
+        NUM_ROWS,
+        None,
+        None,
+    );
+
+    commands.spawn(Camera2dBundle::default());
+
+    const SCALE: f32 = 4.0;
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: tiles_atlas_handle,
+        sprite: TextureAtlasSprite::new(3),
+        transform: Transform::from_scale(Vec3::splat(SCALE)),
+        ..default()
+    });
+}
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(create_battlefield_system)
+        .run();
 }
